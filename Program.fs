baf901ab56670d8cec0473fc7e90a9e429bc418c
Khiem <tong.khiem@gmail.com>
2013-09-20 17:11:05 -0400

    Clean up

--- Program.fs
+++ Program.fs
@@ -17,7 +17,6 @@ let (|Match|_|) pattern input =
     let m = Regex.Match(input, pattern) in
     if m.Success then Some (List.tail [ for g in m.Groups -> g.Value ]) else None
 
-
 let launchProcess args = 
 
     let processStartInfo = new ProcessStartInfo( 
@@ -71,7 +70,7 @@ let main(args) =
     if not <| Seq.isEmpty found then
         eprintfn "%s" "Change set has files with tabs:\r\n"
 
-        found |> Seq.iter (fun invalidFileName -> printFileName invalidFileName)
+        found |> Seq.iter printFileName
 
         1
     else
